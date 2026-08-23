43773dea259e9cb7f77bb1e32f2a0e3bacaaac87
shawansarders-hash <shawansarders@gmail.com>
2026-08-24 00:42:02 +0600

    Add files via upload

--- data.js
+++ data.js
@@ -53,4 +53,11 @@ const SITE = {
     { name: "WhatsApp", icon: "whatsapp", url: "" },
     { name: "Email", icon: "email", url: "" }
   ]
+
+  // Optional image captions. Filename based.
+  achievementInfo: {},
+  momentInfo: {},
+  placeInfo: {},
+  buildInfo: {}
+
 };
